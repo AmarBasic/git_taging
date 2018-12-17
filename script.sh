faf9c628078b838eb31d83e6276212c71705ba59
Amar Basic <amarbasic4@gmail.com>
2018-12-17 14:42:05 +0100

    Script updated

--- script.sh
+++ script.sh
@@ -2,21 +2,29 @@
 
 major_max=1;
 minor_max=0;
-micro_max=0;
+patch_max=0;
 
 branch_name="${BRANCH_NAME}"
 
-$(git fetch --tags)
+echo "Creating tag for branch $BRANCH_NAME"
 
-last_tag=$(git describe --abbrev=0 --tags --exact-match)
+if [ -z "$BRANCH_NAME" ]; then
+    echo 'BRANCH_NAME not provided'
+    exit 1
+fi
+
+git checkout $BRANCH_NAME
+git tag -d $(git tag -l)
+git fetch --tags
+
+last_tag=$(git tag -l "$BRANCH_NAME-$MAJOR_VERSION.$MINOR_VERSION.*" sort=v:refname | tail -n 1)
 
 if [[ $last_tag ]]; then
-
+    echo "Last tag: $last_tag"
     version=$(echo $last_tag | grep -o '[^-]*$')
     major=$(echo $version | cut -d. -f1)
     minor=$(echo $version | cut -d. -f2)
-    micro=$(echo $version | cut -d. -f3)
-
+    patch=$(echo $version | cut -d. -f3)
 
     if [ "$major_max" -lt "$major" ]; then
         let major_max=$major
@@ -26,22 +34,19 @@ if [[ $last_tag ]]; then
         let minor_max=$minor
     fi
 
-    if [ "$micro_max" -lt "$micro" ]; then
-        let micro_max=$micro
+    if [ "$patch_max" -lt "$patch" ]; then
+        let patch_max=$patch
     fi
-
-    echo 'Latest version:' $major_max'.'$minor_max'.'$micro_max
-
-    let micro_max=($micro_max+1)
+    let patch_max=($patch_max+1)
 fi
 
 if [ "$major_max" -ne "${MAJOR_VERSION}" ] || [ "$minor_max" -ne "${MINOR_VERSION}" ]; then
     major_max="${MAJOR_VERSION}"
     minor_max="${MINOR_VERSION}"
-    micro_max=0
 fi
 
-echo 'Switching to new version:' $major_max'.'$minor_max'.'$micro_max
-$(git tag -a $branch_name-$major_max.$minor_max.$micro_max $branch_name -m "Version $major_max.$minor_max.$micro_max")
+echo 'Switching to new version:' $major_max'.'$minor_max'.'$patch_max
+$(git tag -a $branch_name-$major_max.$minor_max.$patch_max $branch_name -m "Version $major_max.$minor_max.$patch_max")
 echo 'Push tag to remote'
-$(git push origin $branch_name-$major_max.$minor_max.$micro_max $branch_name)
+$(git push origin $branch_name-$major_max.$minor_max.$patch_max $branch_name)
+exit 0
